0f199f790235d0ed6a5da23c687cb76bb31864f8
DogacUlgacc <ulgac1907@gmail.com>
2023-09-14 13:01:47 +0300

    h3 silindi

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,6 @@ export default function Card() {
     return (
         <div>
             <h2>Card Title</h2>
-            <h3>h3</h3>
             <p>This is a simple card component.</p>
         </div>
     );
